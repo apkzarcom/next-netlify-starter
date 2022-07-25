ec2916a7483750d0a011e13c68ed09ac459465be
apkzarcom <109984950+apkzarcom@users.noreply.github.com>
2022-07-26 00:10:13 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,375 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+Skip to content
+
+APKZAR.COM
+
+ JOIN OUR TELEGRAM CHANNEL
+ BEST APPS
+ BEST GAMES
+ PAID FOR FREE
+Search
+News
+PUBG MOBILE LITE BOT LOBBY V0.21.1 100% Working
+
+ 
+TWO NEW PLAYSTATION EXCLUSIVES LEAKED, NEXT GEN GTA5 REVEALED, & MORE
+
+ 
+7 Best Website Hosting For United Kingdom In 2022
+1MODY
+
+ 
+
+Latest Updates
+Picsart Photo Editor 
+Picsart Photo Editor
+20.3.0
+·
+Photography
+MOD Premium Unlocked
+Adobe Photoshop Express：Photo Editor Collage Maker 
+Adobe Photoshop Express：Photo Editor Collage Maker
+8.3.979
+·
+Photography
+MOD Premium unlocked
+Daynote
+Daynote
+3.3.3
+·
+Lifestyle
+MOD Premium unlocked
+Textra SMS 
+Textra SMS
+4.52
+·
+Communication
+MOD SMS Pro Donated Full
+YouTube Premium 
+YouTube Premium
+17.28.35
+·
+Video Players & Editors
+MOD Premium Unlocked
+Candy Crush Saga 
+Candy Crush Saga
+1.231.0.3
+·
+Casual
+MOD Unlimited Moves/Lives/Unlocked All Level
+Wild Castle TD: Grow Empire Tower Defense In 2021 
+Wild Castle TD: Grow Empire Tower Defense In 2021
+1.13.1
+·
+Arcade
+MOD Unlimited money
+Zombie Frontier 4 
+Zombie Frontier 4
+1.4.1
+·
+Action
+MOD One Hit/God Mode/Unlimited Ammo)
+The Battle Cats 
+The Battle Cats
+11.7.0
+·
+Casual
+MOD Unlimited Money/XP/Food
+Age Of Railways: Train Tycoon
+Age Of Railways: Train Tycoon
+0.23
+·
+Simulation
+MOD Unlimited money
+Bitdefender Mobile Security & Antivirus 
+Bitdefender Mobile Security & Antivirus
+3.3.177.2032
+·
+Tools
+MOD Premium Unlocked/Ads-Free
+Adobe Premiere Rush: Video 
+Adobe Premiere Rush: Video
+2.4.0.2004
+·
+Video Players & Editors
+MOD Subscription Unlocked
+1
+2
+
+ 
+
+Premium Apps – Latest
+ExpressVPN 
+ExpressVPN
+10.45.0-beta
+·
+Tools
+MOD PREMIUM UNLOCKED [EXCLUSIVE]
+Remini  Pro
+Remini Pro
+3.3.0.202131471
+·
+Photography
+MOD Unlimited Cards
+Speedy Quark VPN - Fast Servers & Secure Proxy 
+Speedy Quark VPN - Fast Servers & Secure Proxy
+1.6.0
+·
+Tools
+MOD Premium Unlocked
+INSTAPRO MOD
+INSTAPRO MOD
+9.15
+·
+Art & Design
+MOD Direct Download
+Hotstar 
+Hotstar
+14.7.5
+·
+Entertainment
+MOD AD-Free/Premium Content/Live IPL
+Alight Motion 
+Alight Motion
+4.2.0
+·
+Video Players & Editors
+MOD Premium Unlocked
+UFO VPN 
+UFO VPN
+2.4.9
+·
+Tools
+MOD Premium Unlocked
+Adobe Lightroom 
+Adobe Lightroom
+7.4.1
+·
+Photography
+MOD Premium unlocked
+1.1.1.1: Faster & Safer Internet 
+1.1.1.1: Faster & Safer Internet
+6.16
+·
+Tools
+MOD Premium Unlocked
+Games Mod – Latest
+Diamond Quest: Don't Rush! 
+Diamond Quest: Don't Rush!
+2.90
+·
+Adventure
+MOD Unlimited Money/Crystal
+Diamond Quest 2: The Lost Temple 
+Diamond Quest 2: The Lost Temple
+1.38
+·
+Puzzle
+MOD Unlimited Gems, Many Lives
+Roblox 
+Roblox
+2.535.277
+·
+Adventure
+MOD Unlimited money, All Unlocked
+Leage Of Ninja Moba
+Leage Of Ninja Moba
+4.0.1
+·
+Role Playing
+MOD Unlimited Money
+Find Diamonds For Mine & Craft 
+Find Diamonds For Mine & Craft
+1.1.14
+·
+Casual
+MOD Premium unlocked
+Minecraft 
+Minecraft
+1.19.0.30
+·
+Arcade
+MOD GOD MODE
+Lords Mobile: Tower Defense 
+Lords Mobile: Tower Defense
+2.82
+·
+Strategy
+MOD Auto Battle/VIP Unlocked
+Bloons TD 6 
+Bloons TD 6
+29.3
+·
+Paid
+MOD Unlimited MONEY/XP
+PK XD 
+PK XD
+0.68.6
+·
+Adventure
+MOD Unlimited Money
+Music And Audio
+Spotify: Listen To Podcasts & Find Music You Love 
+Spotify: Listen To Podcasts & Find Music You Love
+8.7.48.1062
+·
+Music & Audio
+MOD Premium unlocked
+Deezer Music Player: Songs, Playlists & Podcasts 
+Deezer Music Player: Songs, Playlists & Podcasts
+7.0.2.62
+·
+Music & Audio
+MOD Premium Unlocked
+Resso Premium 
+Resso Premium
+1.83
+·
+Music & Audio
+MOD VIP Unlocked
+WavePad Audio Editor Free 
+WavePad Audio Editor Free
+13.04
+·
+Music & Audio
+ Original APK
+JioSaavn Music & Radio 
+JioSaavn Music & Radio
+8.4.1
+·
+Music & Audio
+MOD Premium
+Shazam: Discover Songs & Lyrics In Seconds 
+Shazam: Discover Songs & Lyrics In Seconds
+n/a
+·
+Music & Audio
+ Original APK
+Entertainment
+Hotstar 
+Hotstar
+14.7.5
+·
+Entertainment
+MOD AD-Free/Premium Content/Live IPL
+Block Master For Minecraft PE 
+Block Master For Minecraft PE
+2.9.0
+·
+Entertainment
+ Original APK
+Netflix 
+Netflix
+8.32.0
+·
+Entertainment
+MOD Premium Unlocked
+Meme Generator PRO 
+Meme Generator PRO
+4.6181
+·
+Entertainment
+ Original APK
+Wombo: Make Your Selfies Sing 
+Wombo: Make Your Selfies Sing
+3.0.7
+·
+Entertainment
+MOD Pro Full Premium
+Random Apps
+Pocong Adventure  
+Pocong Adventure
+1.0.0.39
+·
+Adventure
+MOD Unlimited Money
+Frostborn: Action RPG 
+Frostborn: Action RPG
+1.15.56.30610
+·
+Action
+MOD Unlocked/Free Craft
+Score! Hero 
+Score! Hero
+2.75
+·
+Sports
+MOD Unlimited Money
+AZ Screen Recorder - Video Recorder, Livestream 
+AZ Screen Recorder - Video Recorder, Livestream
+5.8.7
+·
+Video Players & Editors
+MOD Premium Unlocked
+Duskwood - Crime & Investigation Detective Story 
+Duskwood - Crime & Investigation Detective Story
+1.9.0
+·
+Role Playing
+MOD (Premium/Credits/Unlocked)
+Hill Climb Racing 2 
+Hill Climb Racing 2
+1.47.4
+·
+Racing
+MOD Unlimited All
+Pedometer 
+Pedometer
+5.39
+·
+Health & Fitness
+MOD Premium Unlocked
+Solo Knight 
+Solo Knight
+1.1.189
+·
+Role Playing
+MOD UNLIMITED ENERGY
+
+ 
+Most View
+Voot, Bigg Boss, Colors TV 
+Voot, Bigg Boss, Colors TV
+4.2.8
+·
+Entertainment
+ Original APK
+Sandbox - Pixel Art Coloring 
+Sandbox - Pixel Art Coloring
+0.3.18
+·
+Entertainment
+MOD All Unlocked
+Amazon Prime Video
+Amazon Prime Video
+3.0.327.11047
+·
+Entertainment
+MOD Premium Unlocked
+YouTube Kids 
+YouTube Kids
+·
+Entertainment
+ Original APK
+Farm Heroes Saga
+Farm Heroes Saga
+5.84.5
+·
+Entertainment
+MOD Unlimited money
+1MODY
+ Useful Sections
+Privacy policy
+Dmca Disclaimer
+Apkyolo
+ About Us
+Contact Us
+About Us
+Instamody
+APKZAR.COM- © 2022 All rights reserved
+
 
       <main>
         <Header title="Welcome to my app!" />
